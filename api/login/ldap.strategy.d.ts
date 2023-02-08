9fd265f2b031b93e93e2449d5d7dbd40c42df6d9
Dmitriy303 <rusnakdima03@gmail.com>
2023-02-08 15:12:54 +0600

    Delete api directory

--- api/login/ldap.strategy.d.ts
+++
@@ -1,6 +0,0 @@
-import * as Strategy from 'passport-ldapauth';
-declare const LdapStrategy_base: new (...args: any[]) => Strategy;
-export declare class LdapStrategy extends LdapStrategy_base {
-    constructor();
-}
-export {};
